4e78a86d73704730c91fda13e7660e06a73ecebb
sauromay <justineislearning@gmail.com>
2022-01-14 13:42:34 +0300

    Addittion of View Page

--- app.js
+++ app.js
@@ -9,6 +9,22 @@ app.get("/search", (req, res) => {
   res.render("search");
 });
 
+app.get("/result/:id", (req, res) => {
+  var movieID = req.params.id;
+  var url =
+    "http://www.omdbapi.com/?i=" + movieID + "&plot=full" + "&apikey=b42bf348";
+  console.log(movieID);
+  request(url, (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      let data = JSON.parse(body);
+      console.log(data);
+      res.render("view", { data: data });
+    } else {
+      console.log(error);
+    }
+  });
+});
+
 app.get("/result", (req, res) => {
   var query = req.query.search;
   var url = "http://www.omdbapi.com/?s=" + query + "&apikey=b42bf348";
